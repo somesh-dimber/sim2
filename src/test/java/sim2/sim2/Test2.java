371cc3cf04c75065d323125b11eff0d8f2edcc57
Somesh <somesh.dimber@t-systems.com>
2019-07-13 15:57:32 +0530

    New Class Added

---
+++ src/test/java/sim2/sim2/Test2.java
@@ -0,0 +1,17 @@
+package sim2.sim2;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Test2 extends TestCase {
+
+	@Test
+	public void simpleTest()
+	{
+		int a=1;
+		int b=2;
+		assertTrue(a+b==3);
+	}
+	
+}
