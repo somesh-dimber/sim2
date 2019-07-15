09bfd8440ff54306cf03010a5b3b6ca6401cc38d
Somesh <somesh.dimber@t-systems.com>
2019-07-15 21:25:25 +0530

    reverted changes

--- vars/gitCheckOut.groovy
+++ vars/gitCheckOut.groovy
@@ -1,5 +1,5 @@
 def call() {
 	
-	["git", "clone", "https://github.com/somesh-dimber/sim2.git", "feature"].execute()
+	["git", "clone", "https://github.com/somesh-dimber/sim2.git", "master"].execute()
 		  }
 		  
