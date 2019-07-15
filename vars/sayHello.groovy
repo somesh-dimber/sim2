ac276a1020a49df82906ab978fbb7fe53725688b
Somesh <somesh.dimber@t-systems.com>
2019-07-15 17:47:09 +0530

    Edited helo

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,4 +2,13 @@
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  
+  echo "Check status"
+  
+	  (1..3).each {
+		  echo "Number: " + it
+	  }
+  
+	  currentBuild.result = 'SUCCESS' //FAILURE to fail
+	  return this
 }
