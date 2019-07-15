8f475d25a6437be7d65989561e9b19315ec0ca41
Somesh <somesh.dimber@t-systems.com>
2019-07-15 18:22:42 +0530

    Edited Helo

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,23 +1,5 @@
 #!/usr/bin/env groovy
-def call(body) {
-	echo "Check status"
 
-	(1..3).each {
-		echo "Number: " + it
-	}
-echo "Inside Hello Statu"
-	currentBuild.result = 'SUCCESS' //FAILURE to fail
-	return this
+def call(String name = 'human') {
+  echo "Hello, ${name}."
 }
-//def call(String name = 'human') {
-//  
-//	echo "Hello, ${name}."
-//  
-//  (1..3).each {
-//	  echo "Number: " + it
-//  }
-//
-//  currentBuild.result = 'SUCCESS' //FAILURE to fail
-//  return this
-//  
-//}
