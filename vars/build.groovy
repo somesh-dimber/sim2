47c95838420d6e47b1ec7feadcad3a1f86dcb0d4
Somesh <somesh.dimber@t-systems.com>
2019-07-15 21:45:49 +0530

    Added build

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,16 @@
-
-class build {
+def call() {
+	
+	
+	mvnHome = tool 'maven3'
+	
+	withEnv(["M2_HOME=$mvnHome"]) {
+		
+		bat(/"%M2_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+	//bat(/"${mvnHome}\bin\mvn" -Dintegration-tests.skip=true clean package/)
+	def pom = readMavenPom file: 'pom.xml'
+	print pom.version
+	//junit '**//*target/surefire-reports/TEST-*.xml'
+	//archive 'target*//*.jar'
+	
+	
 }
