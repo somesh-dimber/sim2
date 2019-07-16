86753db3e2f8d7cbf770d0d1bc65ca6ec5c3157f
Somesh <somesh.dimber@t-systems.com>
2019-07-16 20:13:40 +0530

    In App test

--- src/test/java/sim2/sim2/AppTest.java
+++ src/test/java/sim2/sim2/AppTest.java
@@ -8,11 +8,10 @@ public class AppTest {
 //Adding Comment
 	@Test
 	public void test() {
-<<<<<<< HEAD
+
 		System.out.println("Inside AppTest");
-=======
 		System.out.println("Square Test");
->>>>>>> feature1
+
 		App test =new App();
 		int output = test.square(5);
 		assertEquals(25, output);
