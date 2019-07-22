d8215db9c948cae6914be16381231c980f28a7cc
Somesh <somesh.dimber@t-systems.com>
2019-07-22 17:18:09 +0530

    Modified test

--- vars/buildTest.groovy
+++ vars/buildTest.groovy
@@ -5,13 +5,16 @@ def call() {
 	def pom = readMavenPom file: 'pom.xml'
 	
 	withEnv(["M2_HOME=$mvnHome"]) {
+	
+		echo"Maven Test Initiated for build number=${currentBuild.number}"
 		
-		bat(/"%M2_HOME%\bin\mvn" -Dmaven.test.failure.ignore test package/)
+		bat(/"%M2_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean test package/)
 	//bat(/"${mvnHome}\bin\mvn" -Dintegration-tests.skip=true clean package/)
 //	def pom = readMavenPom file: 'pom.xml'
 	print pom.version
 	//junit '**//*target/surefire-reports/TEST-*.xml'
 	//archive 'target*//*.jar'
+	echo"Maven Test Done for ${currentBuild.number}"
 	
 	}
 }
