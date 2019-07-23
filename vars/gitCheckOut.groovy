d0d10976740b61896a0f1037e21e2aa2f1e9cf7f
Somesh <somesh.dimber@t-systems.com>
2019-07-23 11:33:54 +0530

    added logging to checkout

--- vars/gitCheckOut.groovy
+++ vars/gitCheckOut.groovy
@@ -1,8 +1,9 @@
 def call() {
-	
+	log.info 'Inside Git Checkut'
+	echo"Inside Git Checkout"
 	//["git", "checkout", "https://github.com/somesh-dimber/sim2.git"].execute()
 	git 'https://github.com/somesh-dimber/sim2.git'
-	log.info 'Inside Git Checkut'
+	
 	
 		  }
 		  
